138689161927b227a377dbe3568c81c5702e05cd
Richard Bennett <richardbennett@geekible.org>
2024-10-10 19:37:57 +0100

    User edit

--- gatekeeper/src/components/User/UserEdit/UserEdit.js
+++ gatekeeper/src/components/User/UserEdit/UserEdit.js
@@ -4,11 +4,16 @@ import axios from "axios";
 import Cookies from 'js-cookie';
 
 function UserEdit() {
-    const [username, setUsername] = useState(''); 
-    const [user, setUser] = useState(null);
+    const [usernameSearch, setUsernameSearch] = useState(''); 
+
+    const [userId, setUserId] = useState(0);
+    const [username, setUsername] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [surname, setSurname] = useState('');
 
     function handleUsernameSearch() {
-        const url = 'http://localhost:8080/user/get-by-username?username=' + username;
+        const url = 'http://localhost:8080/user/get-by-username?username=' + usernameSearch;
         const token = Cookies.get('user');
 
         axios.get(url, {
@@ -16,8 +21,28 @@ function UserEdit() {
                 'access_token': token
             }
         }).then(resp => {
-            setUser(resp.data);
-            console.log(resp.data);
+            setUsername(resp.data.Username);
+            setEmailAddress(resp.data.EmailAddress);
+            setUserId(resp.data.UserId);
+            setFirstName(resp.data.FirstName);
+            setSurname(resp.data.Surname);
+        }).catch(err => console.log(err));
+    }
+
+    function handleUpdateUser() {
+        const url = 'http://localhost:8080/user/update-user';
+        const token = Cookies.get('user');
+
+        axios.put(url, {
+            UserId: userId,
+            Username: username,
+            EmailAddress: emailAddress,
+            FirstName: firstName,
+            Surname: surname
+        }, {
+            headers: { 'access_token': token }
+        }).then(res => {
+
         }).catch(err => console.log(err));
     }
 
@@ -30,25 +55,52 @@ function UserEdit() {
             </Breadcrumb>
             <h1>User Edit</h1>
 
-            <div class="input-group mb-3">
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="Username" 
-                    aria-describedby="button-user-search"
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}/>
-                <button 
-                    class="btn btn-outline-secondary" 
-                    type="button" 
-                    id="button-user-search"
-                    onClick={handleUsernameSearch}>Search</button>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <div class="input-group mb-3">
+                    <input 
+                        type="text" 
+                        class="form-control" 
+                        placeholder="Username" 
+                        aria-describedby="button-user-search"
+                        value={usernameSearch}
+                        onChange={e => setUsernameSearch(e.target.value)}/>
+                    <button 
+                        class="btn btn-outline-secondary" 
+                        type="button" 
+                        id="button-user-search"
+                        onClick={handleUsernameSearch}>Search</button>
+                </div>
             </div>
 
-            <div className="row">
-                <p>{user.Username}</p>
-                <label className="form-label" for="username">Username</label>
-                <input id="username" className="form-control" value={user.Username}/>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <div className="row mb-3">
+                    <div className="col-6">
+                        <label className="form-label" for="username">Username</label>
+                        <input id="username" className="form-control" value={username} onChange={e => setUsername(e.target.value)}/>
+                    </div>
+                    <div className="col-6">
+                        <label className="form-label" for="email-address">Email</label>
+                        <input id="email-address" type="email" className="form-control" value={emailAddress} onChange={e => setEmailAddress(e.target.value)}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-6">
+                        <label className="form-label" for="firstname">First Name</label>
+                        <input id="first-name" type="text" className="form-control" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                    </div>
+                    <div className="col-6">
+                        <label className="form-label" for="surname">Surname</label>
+                        <input id="surname" type="text" className="form-control" value={surname} onChange={e => setSurname(e.target.value)}/>
+                    </div>
+                </div>
+
+                <div className="row mt-3">
+                    <div className="col-8"></div>
+                    <div className="col-4">
+                        <button className="btn btn-success float-end" onClick={handleUpdateUser}>Save</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
